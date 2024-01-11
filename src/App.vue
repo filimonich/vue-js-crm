eb404452276a064abc7950383b9cc040a4502258
Sergey Filimonov <srfilimonoff@gmail.com>
2024-01-11 19:22:46 +0300

    feat: connect the component with login and registration

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <template>
-  <MainLayout>
+  <component :is="getLayout()">
     <router-view />
-  </MainLayout>
+  </component>
 </template>
 
 <script setup>
 import MainLayout from "@/layouts/MainLayout";
+import EmptyLayout from "@/layouts/EmptyLayout";
+import { useRoute } from "vue-router";
+import { ref, onMounted } from "vue";
+const route = useRoute();
+const layout = ref(null);
+
+onMounted(() => {
+  layout.value = getLayout();
+});
+
+function getLayout() {
+  return route.meta.layout === "main" ? MainLayout : EmptyLayout;
+}
 </script>
 
 <style lang="scss">
