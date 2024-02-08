890be37aee01718e4b01f15d29a381e274940dea
Sergey Filimonov <srfilimonoff@gmail.com>
2024-02-08 17:18:16 +0300

    feat: add planning

---
+++ src/components/planning/Determinate.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <div v-for="(category, index) in categories" :key="index">
+      <p>
+        <strong>{{ category.name }}: </strong>
+        {{ category.amount }} из {{ category.limit }}
+      </p>
+      <div class="progress">
+        <div
+          class="determinate green"
+          :style="'width: ' + (category.amount / category.limit) * 100 + '%'"
+        ></div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import { useStore } from "vuex";
+import { ref, computed } from "vue";
+
+const store = useStore();
+const state = computed(() => store.state);
+const categories = computed(() => store.state.auth.categories);
+
+console.log("state", state.value);
+console.log("categories", categories.value);
+</script>
--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -2,25 +2,34 @@
   <div>
     <div class="page-title">
       <h3>Планирование</h3>
-      <h4>12 212</h4>
+      <h4>{{ formatCurrency(bill, "RUB") }}</h4>
     </div>
 
-    <section>
-      <div>
-        <p>
-          <strong>Девушка:</strong>
-          12 122 из 14 0000
-        </p>
-        <div class="progress">
-          <div class="determinate green" style="width: 40%"></div>
-        </div>
-      </div>
-    </section>
+    <AsyncDeterminate />
+    
   </div>
 </template>
 
-<script>
-export default {};
-</script>
+<script setup>
+import { useStore } from "vuex";
+import { computed } from "vue";
+import { defineAsyncComponent } from "vue";
+import Loader from "@/components/loader/Loader.vue";
 
-<style></style>
+const store = useStore();
+const bill = computed(() => store.state.auth.bill);
+const formatCurrency = (value, currencyCode = "RUB") => {
+  return new Intl.NumberFormat("ru-RU", {
+    style: "currency",
+    currency: currencyCode,
+    minimumFractionDigits: 2,
+  }).format(value);
+};
+
+const AsyncDeterminate = defineAsyncComponent({
+  loader: () => import("@/components/planning/Determinate.vue"),
+  loadingComponent: Loader,
+  delay: 0,
+  timeout: 3000,
+});
+</script>
