14faa057be32e5cd0d9b7d566d8adb783fb28227
Sergey Filimonov <srfilimonoff@gmail.com>
2024-02-13 14:50:31 +0300

    feat: add button for new category creation

--- src/components/planning/Determinate.vue
+++ src/components/planning/Determinate.vue
@@ -1,28 +1,42 @@
 <template>
-  <section>
-    <div v-for="(category, index) in categories" :key="index">
-      <p>
-        <strong>{{ category.name }}: </strong>
-        {{ formatCurrency(category.amount) }} из
-        {{ formatCurrency(category.limit) }}
-      </p>
-      <div
-        class="progress"
-        v-tooltip="getTooltipText(category.amount, category.limit)"
-      >
-        <div
-          class="determinate"
-          :class="{
-            green: progressWidth(category.amount, category.limit) < 100,
-            red: progressWidth(category.amount, category.limit) === 100,
-          }"
-          :style="{
-            width: `${progressWidth(category.amount, category.limit)}%`,
-          }"
-        ></div>
+  <template v-if="isCategoriesEmpty">
+    <div class="center">
+      <div class="margin">
+        <p class="card-title">Нет категорий</p>
+      </div>
+      <div>
+        <router-link to="/categories">
+          <a class="btn waves-effect waves-light">новая категория</a>
+        </router-link>
       </div>
     </div>
-  </section>
+  </template>
+  <template v-else>
+    <section>
+      <div v-for="(category, index) in categories" :key="index">
+        <p>
+          <strong>{{ category.name }}: </strong>
+          {{ formatCurrency(category.amount) }} из
+          {{ formatCurrency(category.limit) }}
+        </p>
+        <div
+          class="progress"
+          v-tooltip="getTooltipText(category.amount, category.limit)"
+        >
+          <div
+            class="determinate"
+            :class="{
+              green: progressWidth(category.amount, category.limit) < 100,
+              red: progressWidth(category.amount, category.limit) === 100,
+            }"
+            :style="{
+              width: `${progressWidth(category.amount, category.limit)}%`,
+            }"
+          ></div>
+        </div>
+      </div>
+    </section>
+  </template>
 </template>
 
 <script setup>
@@ -31,6 +45,11 @@ import { computed } from "vue";
 
 const store = useStore();
 const categories = computed(() => store.state.auth.categories);
+
+const isCategoriesEmpty = computed(() => {
+  const categories = store.state.auth.categories;
+  return !Array.isArray(categories) || categories.length === 0;
+});
 
 function progressWidth(amount, limit) {
   return (amount / limit) * 100;
