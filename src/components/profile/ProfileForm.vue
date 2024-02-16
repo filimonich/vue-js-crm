013ddb50f36c7b8b52f44099ddd78423ab474757
Sergey Filimonov <srfilimonoff@gmail.com>
2024-02-16 18:52:20 +0300

    feat: add loader for profile

---
+++ src/components/profile/ProfileForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <form class="form">
+    <div class="input-field">
+      <input id="description" type="text" />
+      <label for="description">Имя</label>
+      <span class="helper-text invalid">name</span>
+    </div>
+
+    <button class="btn waves-effect waves-light" type="submit">
+      Обновить
+      <i class="material-icons right">send</i>
+    </button>
+  </form>
+</template>
+
+<script setup></script>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -3,24 +3,18 @@
     <div class="page-title">
       <h3>Профиль</h3>
     </div>
-
-    <form class="form">
-      <div class="input-field">
-        <input id="description" type="text" />
-        <label for="description">Имя</label>
-        <span class="helper-text invalid">name</span>
-      </div>
-
-      <button class="btn waves-effect waves-light" type="submit">
-        Обновить
-        <i class="material-icons right">send</i>
-      </button>
-    </form>
+    <AsyncProfileForm />
   </div>
 </template>
 
-<script>
-export default {};
-</script>
+<script setup>
+import { defineAsyncComponent } from "vue";
+import Loader from "@/components/loader/Loader.vue";
 
-<style></style>
+const AsyncProfileForm = defineAsyncComponent({
+  loader: () => import("@/components/profile/ProfileForm.vue"),
+  loadingComponent: Loader,
+  delay: 0,
+  timeout: 3000,
+});
+</script>
