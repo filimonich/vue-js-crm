44c00de287b47a7d6e285e1ce928e09a5d34e2b7
Sergey Filimonov <srfilimonoff@gmail.com>
2024-02-17 11:00:42 +0300

    fix: update userData, add try catch

--- src/store/actions/userData.js
+++ src/store/actions/userData.js
@@ -2,26 +2,28 @@ import { getAuth } from "firebase/auth";
 import { getDatabase, ref, get } from "firebase/database";
 
 export async function userData(commit) {
-  const auth = getAuth();
-  const user = auth.currentUser;
-  if (user) {
-    const database = getDatabase();
-    const userRef = ref(database, "users/" + user.uid);
-    const snapshot = await get(userRef);
-    if (snapshot.exists()) {
-      const userData = snapshot.val();
-      const name = userData.name;
-      const bill = userData.bill;
-      const categories = userData.categories || {};
-      commit("setUser", { user, name, bill });
-      commit("setCategories", categories);
-      localStorage.setItem(
-        "auth",
-        JSON.stringify({ user, name, bill, categories })
-      );
-    } else {
-      commit("setAuthError", e);
-      throw e;
+  try {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    if (user) {
+      const database = getDatabase();
+      const userRef = ref(database, "users/" + user.uid);
+      const snapshot = await get(userRef);
+      if (snapshot.exists()) {
+        const userData = snapshot.val();
+        const name = userData.name;
+        const bill = userData.bill;
+        const categories = userData.categories || {};
+        commit("setUser", { user, name, bill });
+        commit("setCategories", categories);
+        localStorage.setItem(
+          "auth",
+          JSON.stringify({ user, name, bill, categories })
+        );
+      }
     }
+  } catch (e) {
+    commit("setAuthError", e);
+    throw e;
   }
 }
